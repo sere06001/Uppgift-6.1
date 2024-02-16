0dd00490b9b04b6b73096da8d3639fa0c5dcf7ee
Kreedzz <sean.banan420@gmail.com>
2024-02-16 15:09:47 +0100

    Add project files.

---
+++ Uppgift 6.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace uppgift6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv tal 1: ");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.Write("Skriv tal 2: ");
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.Write("Skriv tal 3: ");
+            int tal3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Addera(tal1, tal2, tal3));
+            Console.ReadKey();
+        }
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            return tal1 + tal2 + tal3;
+        }
+    }
+}
